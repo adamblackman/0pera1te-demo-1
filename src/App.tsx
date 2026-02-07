ed4eb63b7cbb3e43c9befdae913367a1e220baa8
Adam Blackman <112731558+adamblackman@users.noreply.github.com>
2026-02-06 23:14:27 -0500

    feat: wrap App with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { Hero } from './components/Hero';
 import { Capabilities } from './components/Capabilities';
 import { HowItWorks } from './components/HowItWorks';
 import { Footer } from './components/Footer';
+import { ThemeProvider } from './context/ThemeContext';
 
 export default function App() {
   return (
-    <div className="relative min-h-screen">
-      <div className="noise-overlay" />
-      <Navigation />
-      <main>
-        <Hero />
-        <Capabilities />
-        <HowItWorks />
-      </main>
-      <Footer />
-    </div>
+    <ThemeProvider>
+      <div className="relative min-h-screen">
+        <div className="noise-overlay" />
+        <Navigation />
+        <main>
+          <Hero />
+          <Capabilities />
+          <HowItWorks />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
